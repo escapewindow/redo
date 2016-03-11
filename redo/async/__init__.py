6cb4ccb618374111c2102cebef9e4eeb13679703
Aki Sasaki <aki@escapewindow.com>
2016-03-11 11:25:58 -0800

    21 failed, 43 passed

--- redo/async/__init__.py
+++ redo/async/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
 if sys.version_info >= (3, 4):
-    from redo.async.source import retry, retriable, retrying
+    from redo.async.source import retry, retriable, retrying, calculate_sleep_time
 
-__all__ = ['retry', 'retriable', 'retrying']
+__all__ = ['retry', 'retriable', 'retrying', 'calculate_sleep_time']
